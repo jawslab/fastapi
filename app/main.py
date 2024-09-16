58eca0fd9d86f99243fd01a42c13225b5f687e2b
vscicd <vs@jawslab.com>
2024-09-16 13:32:23 -0400

    Alembic is working with Auto Gene

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from fastapi import Body, FastAPI, Response, status, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from random import randrange
 # Note: the module name is psycopg, not psycopg3
@@ -8,11 +9,25 @@ from .database import engine, get_db
 from .routers import post, user, auth, vote
 from .config import settings
 
-models.Base.metadata.create_all(bind=engine)
+
+
+# disable it to let Alembic to create it from models.py
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "https://www.google.com",
+    "http://localhost:8000",  # Add localhost for development
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 my_posts = [
